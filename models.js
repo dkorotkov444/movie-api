4e5b588e615b9512418dae8681e3a523cddd24f6
DK-CF <korotkovadvisory@gmail.com>
2025-09-22 17:22:25 +0200

    A2-E2.08 Defined Mongoose schema and models in models.js

---
+++ models.js
@@ -0,0 +1,49 @@
+/*  models.js
+* JavaScript file with ODM models for the REEL movie API
+* Uses ESM syntax
+*/
+
+// Import required frameworks and modules
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+// Define the user schema
+const userSchema = new Schema({
+    userid: {type: Number},
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    birth_date: { type: Date},
+    favorites: [{ type: Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+// Define the movie schema
+const movieSchema = new Schema({
+    movieid: { type: Number },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    release_year: { type: Number, required: true },
+    image_url: { type: String },
+    rating_imdb: { type: Number },
+    featured: { type: Boolean },
+    starring: [{ type: String }],
+    director: { 
+        name: { type: String, required: true },
+        bio: { type: String },
+        birth_date: { type: Date },
+        death_date: { type: Date }
+    },
+    genre: { 
+        name: { type: String},
+        description: { type: String }
+    }
+});
+
+// Create the user model
+const User = model('User', userSchema);
+
+// Create the movie model
+const Movie = model('Movie', movieSchema);
+
+// Export the models
+export { User, Movie };
